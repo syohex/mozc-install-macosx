a5bdd342112b08d15dd3f3dc48f990f4902a77fa
Syohei YOSHIDA <syohex@gmail.com>
2016-07-08 23:34:49 +0900

    Update for MacOS 10.11

--- mozc_install.sh
+++ mozc_install.sh
@@ -2,35 +2,55 @@
 set -e
 
 mozc_build() {
-	git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-	export PATH=$PWD/depot_tools:$PATH
+    rm -rf work
 
-	mkdir mozc
-	cd mozc
-	gclient config https://github.com/google/mozc.git --name=. --deps-file=src/DEPS
-	gclient sync
+    if [ ! -d gyp ]; then
+        git clone https://chromium.googlesource.com/external/gyp
+    fi
 
-	cd src
-	curl -LO https://gist.githubusercontent.com/syohex/1cd72dccc345b3c7fa45/raw/40875a9b0970bc80202328ba4e195139834baf3f/mozc-macosx.patch
-	patch build_mozc.py < mozc-macosx.patch
-	GYP_DEFINES="mac_sdk=10.10 mac_deployment_target=10.10" python build_mozc.py gyp --noqt
-	python build_mozc.py build -c Release mac/mac.gyp:GoogleJapaneseInput mac/mac.gyp:gen_launchd_confs unix/emacs/emacs.gyp:mozc_emacs_helper
+    export PATH=$PWD/gyp:$PATH
+
+    # Use system python
+    export PATH=/usr/bin:$PATH
+    mkdir work
+    cd work
+    git clone https://github.com/google/mozc.git -b master --single-branch --recursive
+
+    cd mozc/src
+    curl -LO https://gist.githubusercontent.com/syohex/1cd72dccc345b3c7fa45/raw/40875a9b0970bc80202328ba4e195139834baf3f/mozc-macosx.patch
+    patch build_mozc.py < mozc-macosx.patch
+    GYP_DEFINES="mac_sdk=10.11 mac_deployment_target=10.11" python build_mozc.py gyp --noqt
+    python build_mozc.py build -c Release mac/mac.gyp:GoogleJapaneseInput
+    python build_mozc.py build -c Release mac/mac.gyp:gen_launchd_confs
+    python build_mozc.py build -c Release unix/emacs/emacs.gyp:mozc_emacs_helper
 }
 
 mozc_install() {
-	cd mozc/src
+    cd work/mozc/src
 
-	set -x
-	sudo cp out_mac/Release/mozc_emacs_helper /usr/local/bin
-	sudo cp -r out_mac/Release/Mozc.app '/Library/Input Methods/'
-	sudo cp -r out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Converter.plist /Library/LaunchAgents
-	sudo cp out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Renderer.plist /Library/LaunchAgents
-	set +x
+    set -x
+    sudo cp out_mac/Release/mozc_emacs_helper /usr/local/bin
+    sudo cp -r out_mac/Release/Mozc.app '/Library/Input Methods/'
+    sudo cp -r out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Converter.plist /Library/LaunchAgents
+    sudo cp out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Renderer.plist /Library/LaunchAgents
+    set +x
+}
+
+mozc_clean() {
+    rm -rf mozc gyp
+}
+
+mozc_check() {
+    if ! which ninja >/dev/null 2>&1 ; then
+        echo "Please install ninja(ex: brew install ninja)"
+        exit 1
+    fi
 }
 
 if [ "$1" = "" ]; then
-	echo "mozc_install.sh (build|install)"
+	echo "mozc_install.sh (build|install|clean)"
 	exit
 fi
 
+mozc_check
 mozc_"$1"
